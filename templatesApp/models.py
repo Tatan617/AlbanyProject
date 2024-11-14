874831f77330fe18e18d64dd89d05cfb38f83559
AngelSi2 <rodriguezmoreira7777@gmail.com>
2024-11-13 23:35:03 -0300

    avance

---
+++ templatesApp/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Categoria(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'categorias'
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+        ordering = ['id']
+
+class Productos(models.Model):
+    nombre = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to='productos/', blank=True)
+    detalle = models.TextField(max_length=1000, verbose_name='Información del producto')
+    precio = models.FloatField()
+    disponible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'productos'
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['id']
